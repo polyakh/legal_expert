2878774d7bc9ee23217023d9d1db4768aef0d135
polyakh <polyakh.aleksandr@gmail.com>
2023-01-28 18:31:09 +0200

    feat: update Form children

---
+++ components/contacts/contacts-form.tsx
@@ -0,0 +1,47 @@
+//region Local Imports
+import type {ServicesModel} from "~lib/services/types";
+import type {WithAsProps} from "~shared/typings";
+import styles from "./services-list.module.css";
+import {Card, NavLink} from "~components/ui";
+import {PATH_SERVICES} from "~shared/consts";
+
+//endregion
+
+interface ServicesListOwnProps<T> extends WithAsProps {
+  readonly services: T;
+}
+
+const COMPONENT_KEY = "ServicesList";
+
+type ArrayElementType<T> = T extends (infer E)[] ? E : T;
+
+function ServicesList<T extends ServicesModel>({as: Component = "div", services}: ServicesListOwnProps<T>) {
+  const renderServices = () => {
+    return services.map((service) => {
+      return (
+        <NavLink
+          key={service.id}
+          href={`${PATH_SERVICES}[slug]`}
+          as={`${PATH_SERVICES}${service.slug}`}
+        >
+          <Card
+            as={'li'}
+            title={service.title}
+            description={service.description}
+            src={service.url}
+          />
+        </NavLink>
+      )
+    })
+  };
+  return (
+    <Component className='max-w mx-auto'>
+      <ul className={styles.servicesList}>
+        {renderServices()}
+      </ul>
+    </Component>
+  );
+}
+
+export {COMPONENT_KEY};
+export default ServicesList;
---
+++ pages/api/contacts/index.ts
@@ -0,0 +1,26 @@
+//region Global Imports
+import type {NextApiRequest, NextApiResponse} from 'next';
+//endregion
+
+
+//region Local Imports
+import {ServicesModel} from '~lib/services/types';
+import mockServices from '~lib/services/mock-services.json';
+
+//endregion
+
+interface ApiServicesRequest extends NextApiRequest {
+  body: { readonly id?: string };
+}
+
+export default function handler(
+  {method, body: {id: serviceId}}: ApiServicesRequest,
+  res: NextApiResponse<ServicesModel>
+) {
+  if (method === 'GET') {
+    // const findResults = mockServices.find((service) => service.id === serviceId);
+    res.status(200).json(mockServices);
+  } else {
+    res.status(400).json([]);
+  }
+}
